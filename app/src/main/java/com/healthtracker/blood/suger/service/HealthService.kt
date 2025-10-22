109e5684cd9d0d229a0488d968e84e9b0658af54
焦允 <jiaoyun76861590@gmail.com>
2025-10-22 14:28:10 +0800

    feat(服务): 添加健康服务并更新布局和权限
    添加HealthService用于健康监测功能
    为布局中的LinearLayout添加id以便引用
    添加前台服务相关权限声明

---
+++ app/src/main/java/com/healthtracker/blood/suger/service/HealthService.kt
@@ -0,0 +1,8 @@
+package com.healthtracker.blood.suger.service
+
+import android.app.Service
+import android.content.Intent
+
+class HealthService: Service() {
+    override fun onBind(intent: Intent?) = null
+}
