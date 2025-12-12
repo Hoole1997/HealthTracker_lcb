01c676ac116ca7d2d68fdf3e21888fca7dde0c17
焦允 <jiaoyun76861590@gmail.com>
2025-12-12 16:36:32 +0800

    修改版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
     namespace = "com.healthtracker.blood.suger"
 
     defaultConfig {
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 7
+        versionName = "1.0.3.1"
         buildConfig {
             boolean("showLog", showLog)
         }
