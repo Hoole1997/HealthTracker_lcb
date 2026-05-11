d71e29bc883d32adbbe0291bb957145035e63000
rickyyao <lomoliger@hotmail.com>
2026-05-11 13:18:25 -0400

    code+1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
 
     defaultConfig {
         applicationId = appConfig.stringValue("applicationId")
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         if (semanticVersion != null) {
             println("🏷️ [Version] Override VersionName: $semanticVersion")
