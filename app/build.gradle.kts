89a997cb98d959e22daa562472c48aa014121989
rickyyao <lomoliger@hotmail.com>
2026-05-18 17:39:36 +0800

    更新lc版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,12 +79,12 @@ fun loadChannelConfig(
 val internalChannel = loadChannelConfig(
     name = "internal",
     scriptPath = "../scripts/internal.gradle",
-    launcherUnityDependency = "com.launcher.unity:com.leafmotivation.quizguessoncolor-health-main-dev:1.0.0",
+    launcherUnityDependency = "com.launcher.unity:com.leafmotivation.quizguessoncolor-health-main-dev:1.0.1",
 )
 val officialChannel = loadChannelConfig(
     name = "official",
     scriptPath = "../scripts/official.gradle",
-    launcherUnityDependency = "com.launcher.unity:com.blood.sugar.health.diabetes.tool-BloodSugar:1.0.8",
+    launcherUnityDependency = "com.launcher.unity:com.blood.sugar.health.diabetes.tool-BloodSugar:1.0.9",
 )
 
 val semanticVersion = project.findProperty("internalVersionName")
